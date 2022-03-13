73cd4c9b91c632e818e11ae3d91286622106a8c6
s02180519 <monulaqi@mail.ru>
2022-03-13 04:35:52 +0300

    видоизменение кнопки удаления пользователей и добавление выбора пользователей по маске

--- app/static/js/admin/users.js
+++ app/static/js/admin/users.js
@@ -3,7 +3,25 @@ $(document).ready(function () {
     $('.table_row .select-input.content input').on('click', function () {
         $(this).toggleClass('checked');
     });
-    $('#delete_users').on('click', function () {
+    $('.edit-block #mask_for_select').on('focus keyup focusout', function () {
+        if ($(this).val().length > 0)
+            $('.edit-block label').hide();
+        else
+            $('.edit-block label').show();
+    });
+    $('#select_by_mask').on('click',function (){
+        let mask = $('.edit-block #mask_for_select').val().replace(/[^a-zA-Z0-9]/gi, '');
+        $('.edit-block #mask_for_select').val(mask);
+        $('.users-table .table_row .select-input input').removeClass('checked');
+        $('.users-table .table_row .select-input input').prop('checked',false);
+       $('.users-table .table_row').each(function (){
+            if($('.user-name p',this).text().indexOf(mask)+1) {
+                $('.select-input input', this).addClass('checked');
+                $('.select-input input', this).prop('checked',true);
+            }
+       });
+    });
+    $('.edit-block #delete_users').on('click', function () {
         let usersDelete = {'usersDelete': []};
         $('.table_row .select-input.content input.checked').each(function () {
             let userName = $(this).parents('.table_row').children('.user-name').text();
